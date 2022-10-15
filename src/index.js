47496d4de2c851cd8c02f5880387493329daa94a
Ekaterina Silchenko <maksil.17@gmail.com>
2022-10-15 20:55:38 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryList = document.querySelector('.country-list');
+
+import { fetchCountries } from './js/fetchCountries';
+
+fetchCountries()
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => console.log(err));
+
+function markupCreate(arr) {}
