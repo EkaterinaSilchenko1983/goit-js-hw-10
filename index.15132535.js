0143720d51a9b8e02c89f2881ba2887725a1c723
EkaterinaSilchenko1983 <105131083+EkaterinaSilchenko1983@users.noreply.github.com>
2022-10-16 17:51:42 +0000

    Deploying to gh-pages from @ EkaterinaSilchenko1983/goit-js-hw-10@bd01e901f4dd5f0c5048204e8c9a07690d51a613 🚀

--- index.15132535.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;document.querySelector(".country-list");fetch("".concat("https://restcountries.com/v3.1","/name/").concat(n,"?fields=name,capital,population,flags,languages")).then((function(n){if(!n.ok)throw new Error;return n.json()})).then((function(n){console.log(n)})).catch((function(n){return console.log(n)}))}();
-//# sourceMappingURL=index.15132535.js.map
--- index.327a7a43.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".country-list");var e;fetch(`https://restcountries.com/v3.1/name/${e}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error;return e.json()})).then((e=>{console.log(e)})).catch((e=>console.log(e)));
-//# sourceMappingURL=index.327a7a43.js.map
